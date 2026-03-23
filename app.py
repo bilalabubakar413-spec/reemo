ee22693699ffe3f649a555584319aa47eed55a07
Bilal Abubakar Mohamed <bilal.bilal.abubakar.mohamed@hva.nl>
2026-03-23 10:56:53 +0100

    ok

--- app.py
+++ app.py
@@ -3,12 +3,17 @@ import socketserver
 import os
 import sys
 
-PORT = 8000
+PORT = int(os.environ.get("PORT", 8000))
 DIRECTORY = "."
 
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/html/index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
     # Allow custom port handling and logging
     def log_message(self, format, *args):
